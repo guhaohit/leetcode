cf0494770274ff2f117491881d85f5a018987e5c
guhaohit <ghgh_576290605@qq.com>
2015-01-27 18:29:00 +0800

    leetcode 136 Single Number
    利用按位异或来消除相同的元素，留下单独的元素

--- 136.cpp
+++ 136.cpp
@@ -1,1 +1,12 @@
-#include "stdio.h"
+class Solution {
+public:
+    int singleNumber(int A[], int n) {
+        int result=0;
+        for(int i=0;i<n;i++)
+        {
+            result = result ^ A[i];
+        }
+
+        return result;
+    }
+};
