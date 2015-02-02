dfe1457159171fdd2975dc90037114567c90aeb1
guhaohit <ghgh_576290605@qq.com>
2015-02-02 21:00:30 +0800

    Linked List Cycle
    利用两个指针 一个指针一次跳跃2格 一个指针一次跳跃一个 如果存在循环 这两个指针最终会相等 其中每一次跳跃要检验是否为NULL
    如果出现NULL则说明无循环

---
+++ 141.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(head == NULL)
+            return false;
+        ListNode *fast = head;
+        ListNode *low = head;
+        while(true){
+            if(low->next == NULL || fast->next == NULL)
+                return false;
+            low = low->next;
+            fast = fast->next;
+            if(fast->next == NULL)
+                return false;
+            else
+                fast = fast->next;
+
+            if(fast == low)
+                return true;
+        }
+    }
+};
